b1a6db04aaff03bd56c89e309846f64f7f7dee87
D1eG0594 <dagarzonr594@gmail.com>
2024-05-27 16:51:23 -0500

    Agregando primera version de proyecto

--- PA/app.py
+++ PA/app.py
@@ -1,42 +1,55 @@
 import psycopg2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from db.insert_user import insert_user
+from db.user import User_name, User_login_info
+from db.login_user import login_user
 
 app = Flask(__name__)
-
-def db_connect():
-    # Parámetros de conexión
-    dbname = 'db_stim'
-    user = 'postgres'
-    password = 'postgres'
-    host = 'localhost'  
-    port = '5432'  
-    try:     
-        conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-        return conn
-    except psycopg2.Error as e:
-        print("Error al conectar a la base de datos PostgreSQL:", e)
+app.secret_key = '1234'
 
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-def consulta():
+@app.route("/register_user", methods=["POST"])
+def register():
 
-    conn = db_connect()
-
+    user_name = request.form.get("user_name")
+    email = request.form.get("email")
+    password = request.form.get("password")
     
-    cursor = conn.cursor()
+    # Crear objeto User_name y User_login_info
+    user_name = User_name(user_name) 
+    user_login_info = User_login_info(email, password)
 
-    
-    cursor.execute("SELECT app_id, name FROM juegos WHERE price = 0")
+    insert_user(user_name, user_login_info)
 
-    
-    rows = cursor.fetchall()
+    flash('Usuario registrado correctamente', 'success')
+    return render_template('index.html')
 
-    
-    for row in rows:
-        print(row)
 
-    
-    cursor.close()
-    conn.close()
+@app.route("/login_user", methods=["POST"])
+def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
 
+        user_login_info = User_login_info(email, password)
+
+        credentials = login_user(user_login_info)
+
+        
+
+        if credentials:
+            #flash('Inicio de sesión exitoso', 'success')
+            return render_template('store.html')
+        else:
+            flash('Credenciales incorrectas', 'error')
+    return render_template('index.html')        
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ PA/db/user.py
@@ -0,0 +1,8 @@
+class User_name:
+    def __init__(self, user_name):
+        self.user_name = user_name
+
+class User_login_info:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
