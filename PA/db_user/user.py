d3342146a54eec3495f9e953943cbd5075ee2bf5
D1eG0594 <dagarzonr594@gmail.com>
2024-05-28 13:43:50 -0500

    Arreglando cosas

--- PA/db_user/user.py
+++
@@ -1,8 +0,0 @@
-class User_name:
-    def __init__(self, user_name):
-        self.user_name = user_name
-
-class User_login_info:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
